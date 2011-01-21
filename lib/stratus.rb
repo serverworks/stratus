9c3b1dccb3746801d67dd12a7d9f2fd7bdda1061
Junya Ogura <junyaogura@gmail.com>
2011-01-21 20:27:12 +0900

    added require 'active_support' line

--- lib/stratus.rb
+++ lib/stratus.rb
@@ -1,4 +1,9 @@
+# -*- coding: utf-8; mode: ruby; -*-
+
 module Stratus
 end
+
+# rubygems is only needed in 1.8
+require 'rubygems' if RUBY_VERSION[2] == ?8
 
 Dir[File.join(File.dirname(__FILE__), 'stratus/**/*.rb')].sort.each { |lib| require lib }
